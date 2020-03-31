d0c2b93622d7228db9212b86325cf065737a1ddf
Douglas Vinicio <douglasvinicio@Douglas-MacBook-Pro.local>
2020-03-31 03:14:34 -0400

    Comemnts and Reting function added

--- application.py
+++ application.py
@@ -1,15 +1,10 @@
-import os
-
-import requests
-
-from flask import Flask, render_template, url_for, request, flash, redirect, session
+import os ,json , requests
+from flask import Flask, render_template, url_for, session, request, flash, redirect
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
-
 from wtforms import Form, StringField, PasswordField, validators
 from passlib.hash import sha256_crypt
-import json
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"Rq45z\n\xec]/'
@@ -27,7 +22,7 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
-
+#Index route
 @app.route("/")
 def index():
     return render_template('index.html')
@@ -68,6 +63,17 @@ def register():
         return render_template('success.html')
     return render_template('register.html', form=form)
 
+#Logout
+@app.route('/logout')
+def logout():
+
+    #To clear the user id
+    session.clear()
+
+    # Redirect user to the login page
+    return redirect(url_for('login'))
+
+
 # Login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -99,84 +105,136 @@ def login():
     else:
         return render_template('login.html')
 
-#Logout
-@app.route('/logout')
-def logout():
 
-    #To clear the user id
-    session.clear()
-
-    # Redirect user to the login page
-    return redirect(url_for('login'))
-
-
+#Search Route and Function
 @app.route('/search', methods=['GET'])
 def search():
+    #Wildcard
     book = "%" + request.args.get("book") + "%"
     book = book.title()
     books = db.execute("SELECT * FROM books WHERE (isbn) LIKE :book OR (title) LIKE :book OR (author) LIKE :book", {"book": book}).fetchall()
     books_len = len(books)
     if books_len == 0:
-        return render_template("error.html", message = "No books found.")
+        flash('No books found in the database. Try again!' , 'info')
+        return render_template("index.html")
     else:
         return render_template("results.html", books=books)
 
 
-# Single page book - With all info 
+# Single page book - Showing Info from the database and GoodReads API and Submitting reviews function.
 @app.route("/book/<string:book_isbn>", methods=['GET','POST'])
 def book(book_isbn):
-    '''Fecthing single book by isbn'''
-    if request.method == "POST":
 
-        currentUser = session["username"]
+    #Current USer
+    currentUser = session["username"]
+
+    # Book_id by ISBN
+    row = db.execute("SELECT id FROM books WHERE isbn= :isbn",{"isbn": book_isbn})       
+    #Save as a variable
+    bookId = row.fetchone()
+    bookId = bookId[0]
+
+    #Fetching user ID.
+    row2 = db.execute("SELECT id FROM users where username= :username", {"username":currentUser})
+    userId = row2.fetchone()
+    userId = userId[0]
+
+    #Insert Review method.
+    if request.method == "POST":
         # Fetch form data
         rating = request.form.get("rating")
         comment = request.form.get("comment")
-
-        # Book_id by ISBN
-        row = db.execute("SELECT id FROM books WHERE isbn= :isbn",{"isbn": book_isbn})
-
-        #Save as a variable
-        bookId = row.fetchone()
-        bookId = bookId[0]
         # Putting as a Integer to save on the database
         rating = int(rating)
-
         #Checking if the review has been already submitted by the user logged in.
-        review_check = db.execute("SELECT * FROM reviews where username= :username AND book_id = :book_id",
+        review_check = db.execute("SELECT * FROM reviews WHERE username= :username AND book_id = :book_id",
                         {"username":currentUser,
                          "book_id":bookId})
         if review_check.rowcount == 1:
             flash("Review already submitted for this book", 'danger')
             return redirect("/book/"+book_isbn)
 
-
-        db.execute("INSERT INTO reviews (book_id, username, comment, rating) VALUES \
-                       (:book_id, :username, :comment, :rating)",
+        #Inserting review into the Database.
+        db.execute("INSERT INTO reviews (book_id, users_id, username, comment, rating) VALUES \
+                       (:book_id, :users_id, :username, :comment, :rating)",
                        {"book_id": bookId,
+                        "users_id": userId,
                         "username": currentUser,
                         "comment":comment,
                         "rating":rating})
         db.commit()
-
         return redirect("/book/"+ book_isbn)
+
     else:
 
         book = db.execute("SELECT * FROM books WHERE isbn = :isbn", {"isbn": book_isbn}).fetchone()
+        reviews = db.execute("SELECT * FROM reviews JOIN users ON reviews.users_id = users.id WHERE book_id = :book_id", {"book_id":bookId})
 
     # Set up API - Goodreads : KEY = "dCb5gYyxdpbZIEBAJh9fg"
         res = requests.get("https://www.goodreads.com/book/review_counts.json",
                        params={"key": "dCb5gYyxdpbZIEBAJh9fg", "isbns": book_isbn})
+
     #Data from Json API.
         data = res.json()
         avgrating = data["books"][0]["average_rating"]
         rating_work = data["books"][0]["work_ratings_count"]
-    #Fetching book reviews
-
-        return render_template("book.html", book=book, avgrating=avgrating, rating_work=rating_work)
 
 
+    return render_template("book.html", book=book, avgrating=avgrating, rating_work=rating_work, reviews=reviews)
 
+@app.route("/api/<string:isbn>")
+def api(isbn):
+    data=db.execute("SELECT * FROM books WHERE isbn = :isbn",{"isbn":isbn}).fetchone()
+    if data==None:
+        return render_template('404.html')
+    res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "Cdjuz7jTYIwy5Jj9GhY9sw", "isbns": isbn})
+    average_rating=res.json()['books'][0]['average_rating']
+    work_ratings_count=res.json()['books'][0]['work_ratings_count']
+    x = {
+    "title": data.title,
+    "author": data.author,
+    "year": data.year,
+    "isbn": isbn,
+    "review_count": work_ratings_count,
+    "average_score": average_rating
+    }
+    api=json.dumps(x)
+    return render_template("api.json",api=api)
+
+@app.route("/api/<isbn>", methods=['GET'])
+def api_call(isbn):
+
+    # COUNT returns rowcount
+    # SUM returns sum selected cells' values
+    # INNER JOIN associates books with reviews tables
+
+    row = db.execute("SELECT title, author, year, isbn, \
+                    COUNT(reviews.id) as review_count, \
+                    AVG(reviews.rating) as average_score \
+                    FROM books \
+                    INNER JOIN reviews \
+                    ON books.id = reviews.book_id \
+                    WHERE isbn = :isbn \
+                    GROUP BY title, author, year, isbn",
+                    {"isbn": isbn})
+
+    # Error checking
+    if row.rowcount != 1:
+        return jsonify({"Error": "Invalid book ISBN"}), 422
+
+    # Fetch result from RowProxy    
+    tmp = row.fetchone()
+
+    # Convert to dict
+    result = dict(tmp.items())
+
+    # Round Avg Score to 2 decimal. This returns a string which does not meet the requirement.
+    # https://floating-point-gui.de/languages/python/
+    result['average_score'] = float('%.2f'%(result['average_score']))
+
+    return jsonify(result)
+
+    
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(use_reloader=True, debug=True)
